bff23cda62d9348c3b4791b478d7639e0e8f58c9
Fernando Méndez <45081533+FerMdez@users.noreply.github.com>
2021-03-24 21:42:58 +0100

    Cambios estructurales para adaptar la P1 a la P2

---
+++ panel_admin/index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE HTML>
+<?php 
+    session_start();
+
+    require_once('../assets/php/template.php');
+    $template = new Template();
+
+    // IMPORTANTE:
+    //  VERIFICAR QUE ES ADMIN, SI NO, MOSTRAR MENSAJE DE "ERROR"
+?>
+<!--
+    Práctica 2 - Sistemas Web | Grupo D
+    CompluCine - FDI-cines
+-->
+<html lang="es">
+    <!-- Head -->
+    <?php
+        $template->print_head();
+    ?>
+    <body>
+        <!-- Header -->
+        <?php
+            $template->print_header();
+        ?>
+
+        <!-- Main -->
+        <?php
+            $template->print_main();
+        ?>
+
+        <!-- Panel -->
+        <div class="row">
+            <!-- Left Sidebar -->
+            <div class="sidebar left">
+                <ul>
+                <li>Funcionalidad:</li>
+                    <ul>
+                        <li>Subfuncionalidad</li>
+                        <li>Subfuncionalidad</li>
+                    </ul><br />
+                    <li>Ver como:</li>
+                    <ul>
+                        <li>Usuario no registrado</li>
+                        <li>Usuario registrado</li>
+                        <li>Gerente</li>
+                    </ul><br />
+                    <li>Añadir/Editar/Eliminar:</li>
+                    <ul>
+                        <li>Cines</li>
+                        <li>Películas</li>
+                        <li>Promociones</li>
+                        <li>Gerente</li>
+                    </ul>
+                </ul>
+            </div>
+            <!-- Contents -->
+            <div class="row">
+                <div class="column side"></div>
+                    <div class="column middle">
+                        <h2>AQUÍ EL CONTENIDO DE CADA FUNCIONALIDAD.</h2>
+                        <p>Debe variar dinámicamente según el botón del panel izquierdo que se pulse (sin cargar una página diferente, aunque tendrá que recargar el contido, eso sí).</p>
+                        <p>Tendréis que rehacer todo el "PANEL" con PHP.</p>
+                    </div>
+                    <div class="column side"></div>
+                </div>
+            </div>
+
+        <!-- Footer -->
+        <?php
+            $template->print_footer();
+        ?>
+       
+    </body>
+
+</html>
