eb3563bc3794d8da82c7be3694cf879e0eef0f64
Fernando Méndez <45081533+FerMdez@users.noreply.github.com>
2021-04-08 11:03:17 +0200

    Update users_dto_interface.php

--- login/includes/users_dto_interface.php
+++ login/includes/users_dto_interface.php
@@ -1,14 +1,14 @@
 <?php
-    interface UsersDTO {
+    interface UsersInterface {
         public function setId($id);
-		public function getId();
-		public function setName($username);
-		public function getName();
+	public function getId();
+	public function setName($username);
+	public function getName();
         public function setEmail($email);
-		public function getEmail();
-		public function setPass($passwd);
-		public function getPass();
+	public function getEmail();
+	public function setPass($passwd);
+	public function getPass();
         public function setRol($rol);
-		public function getRol();
+	public function getRol();
     }
 ?>
