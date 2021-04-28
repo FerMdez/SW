b3aa890b7e721e602c3f518ff2a3365a30538e50
Fernando Méndez <45081533+FerMdez@users.noreply.github.com>
2021-04-28 21:06:53 +0200

    Add files via upload

--- assets/php/config.php
+++ assets/php/config.php
@@ -1,4 +1,19 @@
 <?php
+    /**
+    * Connection parameters to the DB.
+    */
+    define('BD_HOST', 'localhost');
+    define('BD_NAME', 'complucine');
+    define('BD_USER', 'sw');
+    define('BD_PASS', '_admin_');
+
+    /**
+    * Utf-8 support settings, location (language and country) and time zone.
+    */
+    ini_set('default_charset', 'UTF-8');
+    setLocale(LC_ALL, 'es_ES.UTF.8');
+    date_default_timezone_set('Europe/Madrid');
+
     //Start session:
     session_start();
 
