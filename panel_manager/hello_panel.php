ef7d231707cdb2ad4eaa99d0435b88e9819ede0a
Markines16 <80280295+Markines16@users.noreply.github.com>
2021-05-04 21:13:25 +0200

    Delete hello_panel.php

--- panel_manager/hello_panel.php
+++
@@ -1,9 +0,0 @@
-<div class="column side"></div>
-<div class="column middle">
-	<div class="code info">
-		<h1> Bienvenido al panel de Gerente </h1>
-		<br>
-		<p> Espero que este pasando un buen dia <p>
-	</div>
-</div>
-<div class="column side"></div>
