43c39e67edd42baf5824820609af36bdee6c75f0
Fernando Méndez <45081533+FerMdez@users.noreply.github.com>
2021-05-09 23:51:24 +0200

    Update formChangeName.php

--- panel_user/includes/formChangeName.php
+++ panel_user/includes/formChangeName.php
@@ -19,18 +19,12 @@ class FormChangeName extends Form {
         $errorPassword = self::createMensajeError($errores, 'pass', 'span', array('class' => 'error'));
         $errorPassword2 = self::createMensajeError($errores, 'repass', 'span', array('class' => 'error'));
 
-        $html = "<div class='row'>".$htmlErroresGlobales."
-                            <fieldset id='nombre_usuario'>
+        $html = "<div class='row'>
+                            <fieldset id='nombre_usuario'><pre>".$htmlErroresGlobales."</pre>
                                 <legend>Nuevo Nombre de usuario</legend>
-                                <div class='_new_name'>
-                                    <input type='text' name='new_name' id='new_name' value='' placeholder='Nuevo Nombre' required/><pre>".$errorNombre."</pre>
-                                </div>
-                                <div class='_passwd'>
-                                    <input type='password' name='pass' id='pass' value='' placeholder='Contraseña' required/><pre>".$errorPassword."</pre>
-                                </div>
-                                <div class='_passwd'>
-                                    <input type='password' name='repass' id='repass' value='' placeholder='Repita la contraseña' required/><pre>".$errorPassword2."</pre>
-                                </div>
+                                <input type='text' name='new_name' id='new_name' value='' placeholder='Nuevo Nombre' required/><pre>".$errorNombre."</pre>
+                                <input type='password' name='pass' id='pass' value='' placeholder='Contraseña' required/><pre>".$errorPassword."</pre>
+                                <input type='password' name='repass' id='repass' value='' placeholder='Repita la contraseña' required/><pre>".$errorPassword2."</pre>
                             </fieldset>
                             <div class='actions'> 
                                 <input type='submit' id='submit' value='Cambiar Nombre de Usuario' class='primary' />
@@ -47,12 +41,12 @@ class FormChangeName extends Form {
         $nombre = $datos['new_name'] ?? null;
         $nombre = strtolower($nombre);
         if ( empty($nombre) || mb_strlen($nombre) < 3 ) {
-            $result['new_name'] = "El nombre tiene que tener una longitud de al menos 3 caracteres.";
+            $result['new_name'] = "El nombre tiene que tener\n una longitud de al menos\n 3 caracteres.";
         }
         
         $password = $datos['pass'] ?? null;
         if ( empty($password) || mb_strlen($password) < 4 ) {
-            $result['pass'] = "El password tiene que tener una longitud de al menos 4 caracteres.";
+            $result['pass'] = "El password tiene que tener\n una longitud de al menos\n 4 caracteres.";
         }
         $password2 = $datos['repass'] ?? null;
         if ( empty($password2) || strcmp($password, $password2) !== 0 ) {
@@ -63,42 +57,43 @@ class FormChangeName extends Form {
            $bd = new UserDAO("complucine");
            $user = $bd->selectUser(unserialize($_SESSION['user'])->getName(), $password);
            if (!$user) {
-            $result[] = "Ha ocurrido un probrema al actualizar el nombre de usuario.";
-            $_SESSION['message'] = "<div class='row'>
-                                        <div class='column side'></div>
-                                        <div class='column middle'>
-                                            <div class='code info'>
-                                                <h1>Ha ocurrido un probrema</h1><hr />
-                                                <p>No hemos podido actualizar su nombre de usuario.</p>
-                                                <a href=''><button>Cerrar Mensaje</button></a>
-                                            </div>
-                                        </div>
-                                        <div class='column side'></div>
-                                    </div>
-                                    ";
-        } else {
-            $bd->changeUserName(unserialize($_SESSION['user'])->getId(), $nombre);
-            $user = $bd->selectUser($nombre, $password);
-            if (!$user){
-                $result[] = "Ha ocurrido un probrema al actualizar el nombre de usuario.";
-            }else{
-                $_SESSION['user'] = serialize($user);
-                $_SESSION["nombre"] = $user->getName();
+                $result[] = "Ha ocurrido un problema al actualizar el nombre de usuario.";
                 $_SESSION['message'] = "<div class='row'>
                                             <div class='column side'></div>
                                             <div class='column middle'>
                                                 <div class='code info'>
-                                                    <h1>Operacion realizada con exito</h1><hr />
-                                                    <p>Se ha modificado su nombre de usuario correctamente.</p>
+                                                    <h1>Ha ocurrido un probrema</h1><hr />
+                                                    <p>No hemos podido actualizar su nombre de usuario.</p>
                                                     <a href=''><button>Cerrar Mensaje</button></a>
                                                 </div>
                                             </div>
                                             <div class='column side'></div>
                                         </div>
                                         ";
-                $result = './?option=manage_profile';
+            } else {
+                $user = $bd->selectUser($nombre, $password);
+                if (!$user){
+                    $result[] = "El nombre de usuario ya existe.";
+                } else {
+                    $bd->changeUserName(unserialize($_SESSION['user'])->getId(), $nombre);
+                    $user = $bd->selectUser($nombre, $password);
+                    $_SESSION['user'] = serialize($user);
+                    $_SESSION["nombre"] = $user->getName();
+                    $_SESSION['message'] = "<div class='row'>
+                                                <div class='column side'></div>
+                                                <div class='column middle'>
+                                                    <div class='code info'>
+                                                        <h1>Operacion realizada con exito</h1><hr />
+                                                        <p>Se ha modificado su nombre de usuario correctamente.</p>
+                                                        <a href=''><button>Cerrar Mensaje</button></a>
+                                                    </div>
+                                                </div>
+                                                <div class='column side'></div>
+                                            </div>
+                                            ";
+                    $result = './?option=manage_profile';
+                }
             }
-        }
         }
         return $result;
     }
