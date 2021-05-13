31ba3ec8586262d2b16b0b974a1e3c665977906c
marian1010 <79144276+marian1010@users.noreply.github.com>
2021-05-13 12:39:12 +0200

    Update formAddCinema.php

--- panel_admin/includes/formAddCinema.php
+++ panel_admin/includes/formAddCinema.php
@@ -8,7 +8,7 @@ include_once('../assets/php/form.php');
 class formAddCinema extends Form{
 
     public function __construct(){
-        $op = array("action"=>"./?state=mc">);
+        $op = array("action"=>"./?state=mc");
         parent::__construct('formAddCinema',$op);
     } 
 
