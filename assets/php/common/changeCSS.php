63d9c9a61d21ad657981cbd8bc3648fcd6eb4f05
Fernando Méndez <45081533+FerMdez@users.noreply.github.com>
2021-06-04 23:07:20 +0200

    Add files via upload

--- assets/php/common/changeCSS.php
+++ assets/php/common/changeCSS.php
@@ -4,6 +4,6 @@
     switch(true){
         case strpos($_GET["css"], "main.css"): $_SESSION["css"] = "main.css"; break;
         case strpos($_GET["css"], "highContrast.css"): $_SESSION["css"] = "highContrast.css"; break;
-        default: $_SESSION["css"] = "main.css"; break;
+        //default: $_SESSION["css"] = "main.css"; break;
     }
 ?>
