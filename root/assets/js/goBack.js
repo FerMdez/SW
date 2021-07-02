0e56e727bc202ce5c4f5c6f5285430e549287b22
Fernando Méndez <45081533+FerMdez@users.noreply.github.com>
2021-07-02 18:01:41 +0200

    Delete root directory

--- root/assets/js/goBack.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    document.getElementById('go-back').addEventListener('click', function(event){
-    event.preventDefault();
-    history.back();
-    //window.history.go(-1);
-  });
-});
--- root/assets/php/common/changeCSS-FER_SURFACE.php
+++
@@ -1,9 +0,0 @@
-<?php
-    include('../../../assets/php/config.php');
-
-    switch(true){
-        case strpos($_GET["css"], "main.css"): $_SESSION["css"] = "main.css"; break;
-        case strpos($_GET["css"], "highContrast.css"): $_SESSION["css"] = "highContrast.css"; break;
-        default: $_SESSION["css"] = "main.css"; break;
-    }
-?>
